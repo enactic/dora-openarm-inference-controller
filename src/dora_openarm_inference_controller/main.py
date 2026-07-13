60e4f7e8ce0729a4d38030f6284ec6b56586d060
Abe Tomoaki <abe@clear-code.com>
2026-07-13 10:58:34 +0900

    Rename expected arm status from `ready` to `aligned` (#8)
    With the following commit, `ready` is no longer sent:
    
    https://github.com/enactic/dora-openarm/commit/433263468deed78178f16676b6be41cc5e264d32

--- src/dora_openarm_inference_controller/main.py
+++ src/dora_openarm_inference_controller/main.py
@@ -73,7 +73,7 @@ def main():
             if is_ready():
                 continue
             side = event_id.removeprefix("arm_").removesuffix("_status")
-            if event["value"][0].as_py() == "ready":
+            if event["value"][0].as_py() == "aligned":
                 ready_arms[side] = True
                 if is_ready():
                     start_time_ns = time.monotonic_ns()
